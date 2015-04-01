c65434d73602b82fb486102fbae02883b35497eb
rjong14 <rjong14@student.avans.nl>
2015-04-02 01:33:55 +0200

    test

--- www/js/app.js
+++ www/js/app.js
@@ -6,14 +6,14 @@ angular.module('App', ['ngMaterial', 'ngRoute', 'App.services', 'App.controllers
             $httpProvider.defaults.useXDomain = true;
             $httpProvider.defaults.headers.common = 'Content-Type: application/json';
             delete $httpProvider.defaults.headers.common['X-Requested-With'];
-        $routeProvider
-        .when('/', {
-            controller: 'MainCtrl',
-            templateUrl: 'partials/main.html'
-        })
-        .when('/view', {
-            controller: 'ViewCtrl',
-            templateUrl: 'partials/view.html'
-        })
-        .otherwise({redirectTo: '/'});
+//        $routeProvider
+//        .when('/', {
+//            controller: 'MainCtrl',
+//            templateUrl: 'partials/main.html'
+//        })
+//        .when('/view', {
+//            controller: 'ViewCtrl',
+//            templateUrl: 'partials/view.html'
+//        })
+//        .otherwise({redirectTo: '/'});
     });
