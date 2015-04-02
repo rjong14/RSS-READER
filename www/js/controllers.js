50b6131c5c83de112f9a326138c740b74be3738f
rjong14 <rjong14@student.avans.nl>
2015-04-02 02:24:05 +0200

    test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,19 @@
 angular.module('App.controllers', [])
     .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
-        $scope.status = "It works!";
-        $http.get("https://rssapi.herokuapp.com/").success(function (response) {
-            $scope.data = response
-        })
+//        $scope.status = "It works!";
+//        $http.get("https://rssapi.herokuapp.com/").success(function (response) {
+//            $scope.data = response
+//        })
     }])
     .controller('ViewCtrl', ['$scope', function ($scope) {
         $scope.status = "Also totally works!";
     }])
-    .controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+    .controller('AppCtrl', ['$scope','$http', '$mdSidenav', function($scope, $http, $mdSidenav){
         $scope.lol = "topkek";
+         $scope.status = "It works!";
+        $http.get("https://rssapi.herokuapp.com/").success(function (response) {
+            $scope.data = response
+        });
       $scope.toggleSidenav = function(menuId) {
           console.log("toggleshit")
     $mdSidenav(menuId).open();
